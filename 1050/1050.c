a9d32e05c21ac15de75ca9edf7ccb2000768f856
dwh_1986 <dwh_1986@126.com>
2019-07-06 16:58:18 +0800

    完善代码

--- 1050/1050.c
+++ 1050/1050.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 typedef struct HNode *MaxHeap;
@@ -9,14 +10,18 @@ struct HNode
 {
 	int *data;
 	int size;
-	int capacity
+	int capacity;
 };
 
+MaxHeap CreatHeap(int maxsize);
 
+void BuildMaxHeap(MaxHeap H);	//将一个数组中的数据调整为最大堆
 
+void PercDown(MaxHeap H, int p);	//将H中以p为根节点的堆调整为最大堆
 
+int DeleteMax(MaxHeap H);	//从最大堆中取出最大的元素并删除一个节点
 
-
+bool IsEmpty(MaxHeap H);	//判断是否为空
 
 int main()
 {
@@ -38,12 +43,14 @@ int main()
 
 	}
 
-	int *A;
-	A = (int *)malloc(N * sizeof(int));
+	MaxHeap H;
+
+	H = CreatHeap(N);
 
 	for (int i = 0; i < N; i++)
 	{
-		scanf("%d", &A[i]);
+		scanf("%d", &H->data[i]);
+		H->size++;
 
 	}
 
@@ -65,81 +72,187 @@ int main()
 
 	}
 
-
-
 	//对A进行由大到小排序。考虑练习用最大堆进行排序
 
-
-
-
-
-
+	BuildMaxHeap(H);
+	//for (int i = 0; i < H->size; i++)
+	//{
+	//	printf("%d ", H->data[i]);
+	//	printf("\n");
+	//}
 
 	//将排好序的数填入螺旋矩阵
 
-	int index, cnt = 0;
+	int index;
 	for ( index = 0; index < m; index++)
 	{
 		int i = index, j;
 		for ( j = i; j < n-i; j++)
 		{
-			if (cnt < N)
-			{
-				result[i][j] = A[cnt++];
-			}
+			result[i][j] = DeleteMax(H);
 			
 		}
 
 		j = n - 1 - index;
 		for ( i = index+1; i < m-index; i++)
 		{
-			if (cnt < N)
-			{
-				result[i][j] = A[cnt++];
-			}
+			
+
+			result[i][j] =DeleteMax(H);
+
 			
 		}
 
 		i = m - 1 - index;
 		for ( j = n - 1 - index - 1; j >= index; j--)
 		{
-			if (cnt < N)
-			{
-				result[i][j] = A[cnt++];
-			}
+
+
+			result[i][j] = DeleteMax(H);
+
 			
 		}
 
 		j = index;
 		for ( i = m - 1 - index - 1; i > index; i--)
 		{
-			if (cnt < N)
-			{
-				result[i][j] = A[cnt++];
-			}
+
+			
+			result[i][j] = DeleteMax(H);
+			
 
 		}
 
 	}
 
-	//for (int i = 0; i < m; i++)
-	//{
-	//	for (int j = 0; j < n; j++)
-	//	{
-	//		printf("%d ", result[i][j]);
-	//	}
-	//	printf("\n");
-	//}
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d", result[i][j]);
+			if (j != n-1)
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 
 
 
 
 
-	free(A);
+	free(H->data);
+	free(H);
 	for (int i = 0; i < m; i++)
 	{
 		free(result[i]);
 	}
 	free(result);
 	return 0;
+}
+
+MaxHeap CreatHeap(int maxsize)
+{
+	MaxHeap H = (MaxHeap)malloc(sizeof(struct HNode));
+
+	H->size = 0;
+	H->capacity = maxsize;
+
+	H->data = (int *)malloc(maxsize * sizeof(int));
+
+	return H;
+
+}
+
+void PercDown(MaxHeap H, int p)
+{
+	int parent, child;
+	int x;
+
+	x = H->data[p];
+	for ( parent = p; parent * 2 + 1 < H->size; parent = child)
+	{
+		child = parent * 2 + 1;
+		if (child != H->size-1 && H->data[child] < H->data[child+1])
+		{
+			child++;
+		}
+
+		if (x < H->data[child])
+		{
+			H->data[parent] = H->data[child];
+		}
+		else
+		{
+			break;
+		}
+
+
+	}
+
+	H->data[parent] = x;
+
+
+}
+
+void BuildMaxHeap(MaxHeap H)
+{
+	for (int i = H->size/2 - 1; i >= 0; i--)
+	{
+		PercDown(H, i);
+	}
+}
+
+int DeleteMax(MaxHeap H)
+{
+	if (IsEmpty(H))
+	{
+		return -1;
+	}
+
+	int maxItem, x;
+	int parent, child;
+
+	maxItem = H->data[0];
+	x = H->data[H->size - 1];
+	H->size--;
+
+	for ( parent = 0; parent * 2 + 1 < H->size; parent = child)
+	{
+		child = parent * 2 + 1;
+		if (child != H->size - 1 && H->data[child] < H->data[child+1])
+		{
+			child++;
+		}
+
+		if (x < H->data[child])
+		{
+			H->data[parent] = H->data[child];
+		}
+		else
+		{
+			break;
+		}
+
+
+	}
+
+	H->data[parent] = x;
+
+	return maxItem;
+
+}
+
+
+bool IsEmpty(MaxHeap H)
+{
+	if (H->size > 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
