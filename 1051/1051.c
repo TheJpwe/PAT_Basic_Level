0cd93f3e60e73beda3a9a9e8d0b0125178e6780d
dwh_1986 <dwh_1986@126.com>
2019-07-07 03:13:58 +0800

    修改0附近符号问题

--- 1051/1051.c
+++ 1051/1051.c
@@ -2,12 +2,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
+	double R1, P1, R2, P2;
+	scanf("%lf %lf %lf %lf", &R1, &P1, &R2, &P2);
+	double R, P, A, B;
+	R = R1 * R2;
+	P = P1 + P2;
+	A = R * cos(P);
+	B = R * sin(P);
+
+	if (A<0 && A>=-0.005)
+	{
+		A = 0.00;
+	}
+
+	if (B<0 && B>=-0.005)
+	{
+		B = 0.00;
+	}
 
 
 
+	printf("%.2f%+.2fi", A, B);
 
 	return 0;
 }
